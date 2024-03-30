da720d5ec479aa1fe3cdb5adc33dba881ac5f42d
Mostafa <imostafa330@gmail.com>
2024-03-30 18:25:26 +0100

    improved the delete order flow

--- src/components/Orders/OrderDialoge.tsx
+++ src/components/Orders/OrderDialoge.tsx
@@ -9,10 +9,10 @@ import { Button } from '../ui/button';
 import { Order } from '@/types/types';
 import OrderStatusBar from './OrderStatusBar';
 import useOrder from '@/composables/useOrder';
+import { Link } from 'react-router-dom';
 
 const OrderDialoge = ({ order }: { order: Order }) => {
   const { deleteOrder } = useOrder(order.id);
-
   return (
     <div>
       <DialogContent>
@@ -23,14 +23,20 @@ const OrderDialoge = ({ order }: { order: Order }) => {
             <p>Date: {order.createdAt}</p>
             <p>Status: {order.status}</p>
             <OrderStatusBar orderStatus={order.status} />
-            <DialogClose>
+            <DialogClose disabled={order.status !== 'pending'}>
               <Button
+                disabled={order.status !== 'pending'}
                 variant='outline'
                 onClick={() => deleteOrder()}
               >
                 Delete Order
               </Button>
             </DialogClose>
+            {order.status === 'pending' && (
+              <Link to={`/checkout/${order.id}`}>
+                <Button variant='outline'>Go to Checkout</Button>
+              </Link>
+            )}
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
