c87a32662369296f6e829956786d442aa1407677
Mostafa <imostafa330@gmail.com>
2024-04-07 16:48:53 +0200

    Error page initial UI implemented

--- src/Pages/ErrorPage.tsx
+++ src/Pages/ErrorPage.tsx
@@ -1,8 +1,8 @@
 const ErrorPage = () => {
   return (
-    <div className='flex h-screen justify-center items-center pt-20'>
-      {' '}
-      ErrorPage{' '}
+    <div className='flex flex-col h-screen my-20 mx-20'>
+      <p className='text-5xl font-extrabold mb-6'>Ops...</p>
+      <p className='text-2xl font-semibold'>Something went wrong...</p>
     </div>
   );
 };
