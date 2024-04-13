294fa8039f0063cabae69521c0759620221f97ed
Mostafa <imostafa330@gmail.com>
2024-04-13 17:31:08 +0200

    fixed main contnet in profile page size & margin

--- src/components/Profile/MainContent.tsx
+++ src/components/Profile/MainContent.tsx
@@ -4,7 +4,7 @@ import { Button } from '../ui/button';
 const MainContent = () => {
   const { user } = useUser();
   return (
-    <div className='flex flex-col items-center justify-center w-11/12 md:w-9/12 bg-white rounded-lg my-2'>
+    <div className='flex flex-col items-center justify-center w-11/12 md:w-9/12 h-screen bg-white rounded-lg mt-2'>
       <div className='flex flex-colitems-center justify-around w-full'>
         <p>Email: {user?.email}</p>
         <Button>Please Comfirm your Email</Button>
