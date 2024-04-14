7aadede2dddc801cb8fcc979a28144752b72039b
Mostafa <imostafa330@gmail.com>
2024-04-14 11:20:07 +0200

    improved responsivness

--- src/components/Landing/JoinUs/JoinUs.tsx
+++ src/components/Landing/JoinUs/JoinUs.tsx
@@ -2,9 +2,9 @@ import { Button } from '@/components/ui/button';
 
 const JoinUs = () => {
   return (
-    <div className='flex flex-col gap-4 h-48 bg-coffee-300 px-24 py-10 mt-10'>
-      <p className='text-3xl font-bold text-white'>Join Us</p>
-      <p className='text-xl font-bold text-white'>
+    <div className='flex flex-col gap-4 h-48 bg-coffee-300  px-12 lg:px-24 lg:py-10 py-6 mt-10'>
+      <p className='text-2xl lg:text-3xl font-bold text-white'>Join Us</p>
+      <p className='text-lg lg:text-xl font-bold text-white'>
         Do you want to join us and get the best offers?
       </p>
       <Button
