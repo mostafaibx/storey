4af87461919073352f6442d8f9f8c1ff6b6446a7
Mostafa <imostafa330@gmail.com>
2024-04-17 15:25:40 +0200

    tried to fix the protected routes issue in production

--- src/Pages/LoggedinPrivateRoute.tsx
+++ src/Pages/LoggedinPrivateRoute.tsx
@@ -1,10 +1,22 @@
 import useAuth from '@/composables/useAuth';
+import { useEffect, useState } from 'react';
 import { Outlet, useLocation, Navigate } from 'react-router';
+import LoadingPage from './LoadingPage';
 
 function LoggedinPrivateRoute() {
+  const [isLoading, setIsLoading] = useState(true);
   const location = useLocation();
   const { isLogin } = useAuth();
 
+  useEffect(() => {
+    // Simulate delay in useAuth
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 600); // Adjust timeout as needed
+  }, []);
+  if (isLoading) {
+    return <LoadingPage />;
+  }
   return isLogin() ? (
     <Navigate
       to='/'
