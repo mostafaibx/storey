cfff55c509a38f3704345f00a86d250293c2b929
Mostafa <imostafa330@gmail.com>
2024-04-19 23:30:21 +0200

    added  JSDOC for authFetchHandler

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -5,6 +5,15 @@ export const options = {
   expires: date,
 };
 
+/**
+ * Sends an HTTP request to the specified URL with the given method and body.
+ *
+ * @param {string} url - The URL to send the request to.
+ * @param {string} [method] - The HTTP method to use for the request. Defaults to 'GET'.
+ * @param {object} [body={}] - The request body, serialized as JSON. Defaults to an empty object.
+ * @return {Promise<any>} - A promise that resolves to the response body, parsed as JSON.
+ * @throws {Error} - If the response is not ok, throws an error with the error message from the response.
+ */
 export const authFetchHandler = async (
   url: string,
   method?: string,
