a826f2c28c8781ba02732d69254e816b1de4fa91
Mostafa <imostafa330@gmail.com>
2024-04-21 12:15:36 +0200

    restored cart page content

--- src/Pages/CartPage.tsx
+++ src/Pages/CartPage.tsx
@@ -1,24 +1,21 @@
-/* import CartTable from '@/components/Cart/CartTable';
+import CartTable from '@/components/Cart/CartTable';
 import EmptyCart from '@/components/Cart/EmptyCart';
 import OrderSummary from '@/components/Cart/OrderSummary';
 import useCart from '@/composables/useCart';
- */
+
 const CartPage = () => {
-  /*   const { cart } = useCart();
-  if (cart && cart.items.length === 0) return <EmptyCart />; */
+  const { cart } = useCart();
+  if (cart && cart.items.length === 0) return <EmptyCart />;
 
   return (
     <div className='flex md:flex-row flex-col h-screen w-screen'>
-      <p>Cart</p>
-    </div>
-    /*     <div className='flex md:flex-row flex-col h-screen w-screen'>
       <div className='w-full md:w-8/12 max-h-1/2 md:max-h-screen'>
         <CartTable />
       </div>
       <div className='w-full md:w-4/12 max-h-1/2 md:max-h-screen'>
         <OrderSummary />
       </div>
-    </div> */
+    </div>
   );
 };
 
